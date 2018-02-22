9a69e0972114127de519e7992f8336f27525bbd1
Grandduchy <jchallenger1@hotmail.com>
2018-02-21 19:53:16 -0500

    Fixed Git overwriting files

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 
 using namespace std;
@@ -6,14 +5,5 @@ using namespace std;
 int main()
 {
     cout << "Hello World!" << endl;
-=======
-
-#include <iostream>
-
-#include "Chip8.hpp"
-
-int main(){
-
->>>>>>> 96d294282662aea3e637acfaa3e409e3b6854139
     return 0;
 }
